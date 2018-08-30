bc88a1d9cf3b5dd9f6119d097b87eb44159bdcb9
wangmeng <wangmeng@51jiabo.com>
2018-08-30 18:15:13 +0800

    自定义classload

---
+++ src/main/java/classload/App.java
@@ -0,0 +1,14 @@
+package classload;
+
+public class App {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/classload/MyClassLoader.java
@@ -0,0 +1,43 @@
+package classload;
+
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @author wangmeng
+ */
+public class MyClassLoader extends ClassLoader {
+
+
+
+
+    @Override
+    protected Class<?> findClass(String name) {
+        String myPath = "file:////Users/wangmeng/IdeaProjects/java-learn-demo/target/classes/" + name.replace(".","/") + ".class";
+        System.out.println(myPath);
+        byte[] cLassBytes = null;
+        Path path = null;
+        try {
+            path = Paths.get(new URI(myPath));
+            cLassBytes = Files.readAllBytes(path);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+        Class clazz = defineClass(name, cLassBytes, 0, cLassBytes.length);
+        return clazz;
+    }
+
+    public static void main(String[] args) {
+        MyClassLoader myClassLoader = new MyClassLoader();
+        Class<?> appClass = myClassLoader.findClass("classload.App");
+        System.out.println(appClass.getClassLoader());
+        System.out.println(appClass == App.class);
+
+    }
+
+}
